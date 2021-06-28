20ed7a52597e626af9f6c61d85b77899454ae95b
Anton Andryakov <60717352+Drentmort@users.noreply.github.com>
2021-06-29 00:52:23 +0300

    Add files via upload

---
+++ Graphic2/GraphBox.cs
@@ -0,0 +1,117 @@
+﻿using Graphic.ChartBox;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Graphic
+{
+    public class GraphBox : Control
+    {
+
+        private Matrix transformation = null;
+        private Grid grid = null;
+        private PointF mouseStartLocation = new PointF(-1,-1);
+        private PointF mouseEndLocation;
+        private PointF location = new PointF();
+        private PointF shift;
+
+        private readonly int speedLimit = 15;
+
+        public GraphBox()
+        {
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            transformation = new Matrix();
+            shift = new Point();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            mouseStartLocation = e.Location;
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            mouseEndLocation = e.Location;
+            float signX = (mouseEndLocation.X - mouseStartLocation.X) / 
+                Math.Abs(mouseEndLocation.X - mouseStartLocation.X);
+
+            float signY = (mouseEndLocation.Y - mouseStartLocation.Y) /
+                Math.Abs(mouseEndLocation.Y - mouseStartLocation.Y);
+
+            float xDiff = Math.Abs(mouseEndLocation.X - mouseStartLocation.X) > speedLimit ?
+                speedLimit * signX : mouseEndLocation.X - mouseStartLocation.X;
+
+            float yDiff = Math.Abs(mouseEndLocation.Y - mouseStartLocation.Y) > speedLimit ?
+                speedLimit * signY : mouseEndLocation.Y - mouseStartLocation.Y;
+
+
+            grid.Shift(xDiff, yDiff);
+            Invalidate();
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)   
+        {
+            base.OnMouseMove(e);
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            if (mouseStartLocation != e.Location)
+            {
+                mouseEndLocation = e.Location;
+            }
+
+            float signX = (mouseEndLocation.X - mouseStartLocation.X) /
+                 Math.Abs(mouseEndLocation.X - mouseStartLocation.X);
+
+            float signY = (mouseEndLocation.Y - mouseStartLocation.Y) /
+                 Math.Abs(mouseEndLocation.Y - mouseStartLocation.Y);
+
+            float xDiff = Math.Abs(mouseEndLocation.X - mouseStartLocation.X) > speedLimit ?
+                speedLimit * signX : mouseEndLocation.X - mouseStartLocation.X;
+            float yDiff = Math.Abs(mouseEndLocation.Y - mouseStartLocation.Y) > speedLimit ?
+                speedLimit * signY : mouseEndLocation.Y - mouseStartLocation.Y;
+
+
+            grid.Shift(xDiff, yDiff);
+            mouseStartLocation = mouseEndLocation;
+            Invalidate();
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            location = e.Location;
+            if (!Bounds.Contains(e.Location))
+                return;
+            location.X -= Bounds.X;
+            location.Y -= Bounds.Y;
+            float K = e.Delta > 0 ? 1.05f : 0.95f;
+            grid.Scale(new PointF(K, K), e.Location);           
+
+            Invalidate();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.DrawRectangle(Pens.Black, new Rectangle(0, 0, Bounds.Width-1, Bounds.Height-1));
+            grid = (grid == null) ? new Grid(Bounds, 10) : grid;
+            grid.Draw(e.Graphics);
+            
+        }
+
+    }
+}
---
+++ Graphic2/MyList/Mycollection.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Graphic.MyList
+{
+    public class MyList<T> : IList<T>
+    {
+        public event Action<T> ItemAdded;
+        public event Action<T> ItemChanged;
+        public event Action<T> ItemDeleted;
+        public event Action<int> CollectionChanged;
+        
+        private readonly IList<T> _list = new List<T>();
+
+        #region Implementation of IEnumerable<T>
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Implementation of ICollection<T>
+
+        public void Add(T item)
+        {
+            if(ItemAdded!=null)
+                ItemAdded.Invoke(item);
+            _list.Add(item);
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return _list.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _list.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            if (ItemDeleted != null) 
+                ItemDeleted.Invoke(item);
+            return _list.Remove(item);
+        }
+
+        public int Count
+        {
+            get { return _list.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return _list.IsReadOnly; }
+        }
+
+        #endregion
+
+        #region Implementation of IList<T>
+
+        public int IndexOf(T item)
+        {
+            return _list.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            _list.Insert(index, item);
+            CollectionChanged.Invoke(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (ItemDeleted != null)
+                ItemDeleted.Invoke(_list[index]);
+            _list.RemoveAt(index);
+        }
+
+        public T this[int index]
+        {
+            get 
+            {
+                return _list[index]; 
+            }
+            set 
+            {
+                _list[index] = value;
+                if (ItemChanged != null)
+                    ItemChanged.Invoke(_list[index]);
+            }
+        }
+
+        #endregion
+
+        #region Your Added Stuff
+
+        public void Update()
+        {
+            foreach(T _obj in _list)
+            {
+                if (ItemChanged != null)
+                    ItemChanged.Invoke(_obj);
+            }
+        }
+
+        #endregion
+    }
+}
